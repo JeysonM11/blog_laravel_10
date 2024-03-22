1ccedb42a8ad1eed6fb708722c74b11dd3a4a889
Jeyson Miranda <jeis11@hotmail.com>
2024-03-22 09:59:39 -0500

    agregando plantilla de AdminLTE

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'Baretos')
+
+@section('content_header')
+    <h1>Baretos y mas</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@stop

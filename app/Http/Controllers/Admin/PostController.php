164b98bd446fe88c1c46b76a9ca9d8a350fcfeee
Jeyson Miranda <jeis11@hotmail.com>
2024-04-15 13:40:21 -0500

    Vista de administrador, crear post

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
+use App\Models\Tag;
 
 class PostController extends Controller
 {
@@ -20,8 +22,12 @@ class PostController extends Controller
      * Show the form for creating a new resource.
      */
     public function create()
-    {
-        return view('admin.posts.create');
+    {   
+        //Llamar registros y pasarlos a formato laravle colletive
+        $categories = Category::pluck('name', 'id');
+        $tags = Tag::all();
+
+        return view('admin.posts.create', compact('categories', 'tags'));
     }
 
     /**
